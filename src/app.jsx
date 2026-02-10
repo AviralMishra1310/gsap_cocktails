c787b6c52912d4b0bd11a83415e087fc0fd519ad
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-02-11 00:48:01 +0530

    Fixed case-sensitive imports

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,11 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from 'gsap';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
-import Cocktails from "./components/cocktails.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
-import Menu from "./components/menu.jsx";
-import Contact from "./components/contact.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
